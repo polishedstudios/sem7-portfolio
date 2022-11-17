40528e40d66463c7f58a07224474e0680d1fcf13
Polished Studios <fd.github@outlook.com>
2022-11-17 22:46:34 +0100

    Add Group MVP Navigation folder

---
+++ Group/4. Realisation/1 MVP Navigation/Source Code Files/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'cubit/bottom_nav_cubit.dart';
+import 'pages/basket_page.dart';
+import 'pages/catalogue_page.dart';
+import 'pages/profile_page.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (context) => BottomNavCubit(),
+        child: MaterialApp(
+          theme: ThemeData(
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: const MainPage(),
+        ));
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final _pageNavigation = [
+    BasketPage(),
+    CataloguePage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BottomNavCubit, int>(
+      builder: (context, state) {
+        return Scaffold(
+          body: _buildBody(state),
+          bottomNavigationBar: _buildBottomNav(),
+        );
+      },
+    );
+  }
+
+  Widget _buildBody(int index) {
+    return _pageNavigation.elementAt(index);
+  }
+
+  Widget _buildBottomNav() {
+    return BottomNavigationBar(
+      currentIndex: context.read<BottomNavCubit>().state,
+      type: BottomNavigationBarType.fixed,
+      onTap: _getChangeBottomNav,
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_basket_outlined), label: "Basket"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search_outlined), label: "Catalogue"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline), label: "Profile"),
+      ],
+      iconSize: 33,
+      unselectedFontSize: 13,
+      selectedFontSize: 13,
+      backgroundColor: Colors.white,
+      selectedItemColor: const Color(0xffEEB717),
+    );
+  }
+
+  void _getChangeBottomNav(int index) {
+    context.read<BottomNavCubit>().updateIndex(index);
+  }
+}
