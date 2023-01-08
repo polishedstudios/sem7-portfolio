8431f9533ac2922ba9bdccb06bad92addb2196c8
Polished Studios <fd.github@outlook.com>
2023-01-08 22:04:46 +0100

    Made challenge object and a challenge.

--- Individual/4. Realisation/Source Code/cookingnator/lib/src/challenge_selection/challenges/challenge.dart
+++ Individual/4. Realisation/Source Code/cookingnator/lib/src/challenge_selection/challenges/challenge.dart
@@ -1,14 +1,4 @@
-class Challenge {
-  final String cuisine;
-  final DishName dishName;
-  // unit?, steps: images, instructions, etc..
-
-  const Challenge({
-    required this.cuisine,
-    required this.dishName,
-    // ...
-  });
-}
+import 'package:flutter/material.dart';
 
 class DishName {
   final String name;
@@ -19,5 +9,42 @@ class DishName {
     required this.name,
     this.original,
     this.originalPronounciation,
+  });
+}
+
+class DishStep {
+  final String type;
+  final String title;
+  final String instruction;
+  // video/image (outside scope)
+  // (optional presentations)
+
+  const DishStep({
+    required this.type,
+    required this.title,
+    required this.instruction,
+  });
+}
+
+class Challenge {
+  final DishName dishName;
+  final Image image;
+  final int difficulty;
+  final Duration duration;
+  final List<Challenge>?
+      upgradeChallenges; // if the challenge has "upgrade" options
+  final Image ingredientsImage;
+  final List<String> ingredients;
+  final List<DishStep> steps;
+
+  const Challenge({
+    required this.dishName,
+    required this.image,
+    required this.difficulty,
+    required this.duration,
+    this.upgradeChallenges,
+    required this.ingredientsImage,
+    required this.ingredients,
+    required this.steps,
   });
 }
--- Individual/4. Realisation/Source Code/cookingnator/lib/src/challenge_selection/challenges/challenges_jp.dart
+++ Individual/4. Realisation/Source Code/cookingnator/lib/src/challenge_selection/challenges/challenges_jp.dart
@@ -1,12 +1,58 @@
+import 'package:flutter/material.dart';
 import 'challenge.dart';
 
 const challenges = [
   Challenge(
-    cuisine: 'Japanese',
     dishName: DishName(
       name: 'Curry Rice',
       original: 'カレーライス',
       originalPronounciation: 'Karēraisu',
     ),
+    image: Image(image: AssetImage('assets/challenges/CurryRice.png')),
+    difficulty: 2,
+    duration: Duration(minutes: 30),
+    ingredientsImage:
+        Image(image: AssetImage('assets/challenges/CurryRice_Ingredients.png')),
+    ingredients: [
+      "400g onion",
+      "250g potato",
+      "100g carrot",
+      "1tbsp oil",
+      "100g curry blocks or curry mix",
+      "800ml water",
+      "4 cups of rice",
+    ],
+    steps: [
+      DishStep(
+          type: "prep",
+          title: "Prepare ingredients",
+          instruction: "Cut all ingredients into cubes of 2cm."),
+      DishStep(
+          type: "cook",
+          title: "Cook rice and set aside",
+          instruction:
+              "Cook the rice and set aside. While cooking the rice, continue with the next steps."),
+      DishStep(
+          type: "cook",
+          title: "Sauté onion pieces",
+          instruction: "Sauté onion pieces until the edges start browning."),
+      DishStep(
+          type: "cook",
+          title: "Add potato and carrot pieces",
+          instruction: "Add potato and carrot pieces to the pot and stir."),
+      DishStep(
+          type: "cook",
+          title: "Add water and bring it to a boil",
+          instruction: "Add water to the pot and cook until it boils."),
+      DishStep(
+          type: "cook",
+          title: "Add curry roux and cook",
+          instruction: "Add the curry roux and cook 5 minutes."),
+      DishStep(
+          type: "presentation",
+          title: "Present your dish",
+          instruction:
+              "Divide half of the plate with rice and the other half with curry."),
+    ],
   ),
 ];
